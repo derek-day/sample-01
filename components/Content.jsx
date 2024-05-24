8617246a17bbd8f518cf99e202122b7abd5315d1
Derek Day <derek.andrew.day@gmail.com>
2024-05-24 17:55:55 -0400

    r

--- components/Content.jsx
+++ components/Content.jsx
@@ -1,10 +1,14 @@
+"use client";
+
 import React from 'react';
 import { Row, Col } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import contentData from '../utils/contentData';
 
+
 const Content = () => (
+
   <div className="next-steps my-5" data-testid="content">
     <h2 className="my-5 text-center" data-testid="content-title">
       What can I do next?
--- next.config.js
+++ next.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   poweredByHeader: false
 };
+
+// module.exports = (phase, { defaultConfig }) => {
+//   return {
+//     ...defaultConfig,
+
+//     webpack: (config) => {
+//       config.resolve = {
+//         ...config.resolve,
+//         fallback: {
+//           "fs": false,
+//           "path": false,
+//           "os": false,
+//         }
+//       }
+//       return config
+//     },
+//   }
+// }
