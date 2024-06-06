0507bf3715147bd9772645d10570c902e1d16508
Derek Day <derek.andrew.day@gmail.com>
2024-06-06 15:54:23 -0400

    check for login on home page

--- components/Content.jsx
+++ components/Content.jsx
@@ -5,6 +5,8 @@ import { Row, Col } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import contentData from '../utils/contentData';
+import { useUser } from '@auth0/nextjs-auth0/client';
+
 
 // import { images } from '../utils/imageLoader';
 
@@ -12,7 +14,10 @@ import contentData from '../utils/contentData';
 // pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 
-const Content = () => (
+const Content = () => {
+  const { user, isLoading } = useUser();
+
+  return (
 
   <div className="next-steps my-5" data-testid="content">
 
@@ -38,27 +43,34 @@ const Content = () => (
     <h2 className="my-4 text-center" data-testid="content-title">
       Rep Folders
     </h2>
-    <div className='mt-5 text-center' data-testid="pdf-items" style={{textAlign:'center'}}>
-            <h4 className="my-4">
-              <a href="/comdue">Comdue</a>
-            </h4>
-            <h4 className="my-4">
-              <a href="/daily">Daily</a>
-            </h4>
-            <h4 className="my-4">
-              <a href="/rcp">Rep Commissions</a>
-            </h4>
-            <h4 className="my-4">
-              <a href="/sample">Sample Bag</a>
-            </h4>
-            <h4 className="my-4">
-              <a href="/territory">Territory Listings</a>
-            </h4>
-            <h4 className="my-4">
-              <a href="/ytd">YTD Critical</a>
-            </h4>
+    
+    {user && (
+    <>
+      <div className='mt-5 text-center' data-testid="pdf-items" style={{textAlign:'center'}}>
+        <h4 className="my-4">
+          <a href="/comdue">Comdue</a>
+        </h4>
+        <h4 className="my-4">
+          <a href="/daily">Daily</a>
+        </h4>
+        <h4 className="my-4">
+          <a href="/rcp">Rep Commissions</a>
+        </h4>
+        <h4 className="my-4">
+          <a href="/sample">Sample Bag</a>
+        </h4>
+        <h4 className="my-4">
+          <a href="/territory">Territory Listings</a>
+        </h4>
+        <h4 className="my-4">
+          <a href="/ytd">YTD Critical</a>
+        </h4>
       </div>
+    </>
+    )}
+
   </div>
-);
+  )
+};
 
 export default Content;
